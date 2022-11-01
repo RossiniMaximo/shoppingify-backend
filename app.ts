1a420082432357bcd7c4626484944036d43c0419
Maximo <maximorossini2016@gmail.com>
2022-11-01 11:58:57 -0300

    aa

--- app.ts
+++ app.ts
@@ -1,11 +1,11 @@
-import * as express from "express";
+import express from "express";
 import itemsRoutes from "./routes/items.routes";
 import listsRoutes from "./routes/lists.routes";
 import myRoutes from "./routes/me.routes";
 import userRoutes from "./routes/user.routes.";
-import * as cors from "cors";
-import path = require("path");
+import cors from "cors";
 const app = express();
+
 const corsOptions = {
   origin: "https://shoppingify-front.vercel.app",
   optionsSuccessStatus: 200,
@@ -15,9 +15,4 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use([itemsRoutes, listsRoutes, myRoutes, userRoutes]);
 
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: path.join(__dirname, "public") });
-});
 export { app };
