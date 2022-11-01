4c73057ba289ed2dc16584392557689bad097e84
Maximo <maximorossini2016@gmail.com>
2022-11-01 12:17:00 -0300

    added dialectModule to Sequelize instance2

--- database/index.js
+++ database/index.js
@@ -6,5 +6,6 @@ require("dotenv").config();
 exports.sequelize = new sequelize_1.Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
     host: process.env.DATABASE_HOST,
     dialect: "mysql",
+    dialectModule: require("mysql2"),
     port: 7022,
 });
