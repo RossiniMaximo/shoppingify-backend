34eb9f81313551f28e51cffd7352d9b23b55bff3
Maximo <maximorossini2016@gmail.com>
2022-11-01 11:46:05 -0300

    Please

--- database/algolia.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.index = void 0;
-const algoliasearch_1 = require("algoliasearch");
-require("dotenv").config();
-const client = (0, algoliasearch_1.default)(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY);
-const index = client.initIndex(process.env.ALGOLIA_INDEX);
-exports.index = index;
