01319a0f637b7173dd86f822edc4550034c5ae1f
Maximo <maximorossini2016@gmail.com>
2022-11-01 12:19:36 -0300

    added public folder and static path

--- app.ts
+++ app.ts
@@ -4,8 +4,8 @@ import listsRoutes from "./routes/lists.routes";
 import myRoutes from "./routes/me.routes";
 import userRoutes from "./routes/user.routes.";
 import cors from "cors";
+import path = require("path");
 const app = express();
-
 const corsOptions = {
   origin: "https://shoppingify-front.vercel.app",
   optionsSuccessStatus: 200,
@@ -15,4 +15,9 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use([itemsRoutes, listsRoutes, myRoutes, userRoutes]);
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
+});
 export { app };
