c429c8d33fde0f6e54646848c5437b0a949d7c10
Maximo <maximorossini2016@gmail.com>
2022-11-07 14:27:28 -0300

    re-updating items to Railway db

---
+++ items.ts
@@ -0,0 +1,149 @@
+export const items = [
+  {
+    name: "Roast Beef",
+    img: "",
+    description:
+      "es un corte de buey tierno que se asa al horno. En la cocina inglesa, y por extensión en muchos países de tradición anglosajona como Australia, el rosbif es un plato muy tradicional, servido preferentemente los domingos (Sunday roast). El rosbif se suele servir con verduras y patatas (fritas, asadas o en puré), y en el norte de Inglaterra se acompaña tradicionalmente de Yorkshire pudding.",
+    category: "Meat & Fish",
+  },
+  {
+    name: "Carne picada",
+    img: "",
+    description:
+      "es una preparación de la carne con objetivos culinarios, para la cual se desmenuzan y se cortan finamente los músculos, grasas y nervios mediante máquina de picar carne, cuchillo (tajadera), etc.",
+    category: "Meath & fish",
+  },
+  {
+    name: "Filet de Merluza",
+    img: "",
+    description:
+      "La merluza pertenece al grupo de los pescados blancos o magros por su bajo aporte de grasas, que apenas alcanza el 2% total. Es por esto por lo que la este pescado blanco forma parte de los alimentos que, de acuerdo con las definiciones regulatorias, está catalogado como alimento de “bajo contenido en grasa”. Sólo consumirás 0,7 gramos en cada ración de 100 gramos. Además, aporta pocas calorías; 72 kcal por cada 100 gramos y 108 kcal por cada ración media de 150 gramos.",
+    category: "Meat & Fish",
+  },
+  {
+    name: "Calamar",
+    img: "",
+    description:
+      "Los calamares son muy nutritivos y una gran fuente de proteínas, necesarias para mantener los músculos sanos. Aunque su contenido en colesterol es algo alto, por lo que una o dos raciones a la semana son buenas cantidades para disfrutar de los diferentes beneficios del calamar, sin que suponga un problema.",
+    category: "Meat & Fish",
+  },
+  {
+    name: "Medallones de pollo",
+    img: "",
+    description:
+      "La carne de pollo también contiene vitamina A, zinc, tiamina, fósforo y ácido fólico. Dependiendo de la pieza del pollo existen diferencias nutricionales, así por ejemplo la pechuga sin piel es la pieza con menor grasa, por lo tanto, tiene menos aporte de colesterol.",
+    category: "Meat & Fish",
+  },
+  {
+    name: "Arvejas",
+    img: "",
+    description:
+      "Son alimentos de origen vegetal, propios de la naturaleza, ya que no están industrializadas ni procesadas. Pertenecen al grupo de los carbohidratos almidonados, debido a que es el nutriente que más prevalece. Además, contienen fibras, proteínas, vitaminas, minerales y son muy bajas o pobres en grasas.",
+    category: "Vegetables",
+  },
+  {
+    name: "Zanahoria",
+    img: "",
+    description:
+      "Es una de las hortalizas más producidas y consumidas en el mundo. Destaca en su composición el betacaroteno, que el  organismo convierte en vitamina A o retinol, lo que le confiere un poder antioxidante con beneficios probados en la regeneración celular, especialmente en la piel. ",
+    category: "Vegetables",
+  },
+  {
+    name: "Tomate",
+    img: "",
+    description:
+      "El tomate está compuesto principalmente por agua y su macronutriente mayoritario son los hidratos de carbono.Entre las vitaminas cabe destacar el contenido en vitamina A.",
+    category: "Vegetables",
+  },
+  {
+    name: "Cebolla",
+    img: "",
+    description:
+      "s un alimento muy antiguo, y entre sus capas esconde numerosas propiedades nutritivas y medicinales, es rica en minerales y oligoelementos (calcio, magnesio, cloro, cobalto, cobre, hierro, fósforo, yodo, níquel, potasio, silicio, cinc, azufre y bromo); y también en vitaminas (A, B, C y E).",
+    category: "Vegetables",
+  },
+  {
+    name: "Choclo",
+    img: "",
+    description:
+      "Este alimento, al ser fuente de carbos y pobre en fibra, posee una carga glucémica alta, aumentando rápidamente la glucosa en sangre, y, por lo tanto, la insulina. Por lo que elevar demasiado la glucosa sanguínea facilita el almacenamiento de grasa.",
+    category: "Vegetables",
+  },
+  {
+    name: "Puerro",
+    img: "",
+    description:
+      "65 gramos por cada 100 gramos de producto fresco. Proteínas, fibra, folatos, vitamina C y B6. El agua es el componente mayoritario de este alimento. Además, es fuente de fibra y de algunas vitaminas (folatos, vitamina C, A y B6).",
+    category: "Vegetables",
+  },
+  {
+    name: "Manzana",
+    img: "",
+    description:
+      "Contiene flavonoides y polifenoles por lo que es rica en antioxidantes, en vitaminas del grupo B (B1, B2 y B6), vitamina C, fósforo, potasio y calcio.",
+    category: "Fruits",
+  },
+  {
+    name: "Naranja",
+    img: "",
+    description:
+      "Como es sabido, es rica en vitamina C, que interviene en la formación de colágeno, huesos y dientes, glóbulos rojos y favorece la absorción del hierro de los alimentos y la resistencia a las infecciones. ",
+    category: "Fruits",
+  },
+  {
+    name: "Uvas",
+    img: "",
+    description:
+      "Su aporte en fibras provoca mayor trabajo digestivo, lo que genera mayor saciedad y colabora con el correcto funcionamiento del aparato digestivo. Además, ayuda a que la glucosa y la insulina no aumenten rápidamente en sangre.",
+    category: "Fruits",
+  },
+  {
+    name: "Banana",
+    img: "",
+    description:
+      "Aporta pocas calorías, una gran cantidad de agua (lo que facilita la hidratación de nuestro cuerpo), fibras, azúcares, vitamina y minerales. Es una fruta rica en potasio, por lo que ayuda a la función nerviosa y muscular (previniendo calambres después del ejercicio).",
+    category: "Fruits",
+  },
+  {
+    name: "Sandia",
+    img: "",
+    description:
+      " Pertenece al grupo de las frutas y contiene antioxidante, vitaminas (carotenos, B) y minerales.Uno de sus beneficios es su gran aporte en agua, siendo este del 90%, ideal como hidratante. Debido a su aporte de líquido es una fruta baja en calorías. ",
+    category: "Fruits",
+  },
+  {
+    name: "Gaseosa sabor cola",
+    img: "",
+    description:
+      "Una bebida de cola es un refresco usualmente saborizado con caramelo colorado, y que frecuentemente posee cafeína.​Creada por el farmacéutico John Stith Pemberton, originalmente se vendía en farmacias.",
+    category: "Beverages",
+  },
+  {
+    name: "Gaseosa sabor lima",
+    img: "",
+    description:
+      "Refresco hecho a base de agua carbonatada con sabor a lima o limón, incolora y sin cafeína.",
+    category: "Beverages",
+  },
+  {
+    name: "Agua",
+    img: "",
+    description:
+      "El agua potable mineral que contiene magnesio y calcio en cantidades adecuadas ayuda a disminuir la concentración de oxalato de calcio. Los estudios también han revelado que el agua mineral puede reducir considerablemente el riesgo de sufrir una piedra de riñón ácido úrico.",
+    category: "Beverages",
+  },
+  {
+    name: "Leche",
+    img: "",
+    description:
+      "Es rica en proteínas y azúcares, importantes para el organismo. Entre las proteínas destaca la caseína, diversas seroglobulinas y albúminas, así como otras de utilidad como la lactoferrina.",
+    category: "Dairy products",
+  },
+  {
+    name: "Queso",
+    img: "",
+    description:
+      "Alimento rico en vitaminas A y D, ambas vitaminas ayudan al cuerpo a absorber el calcio y a mantener los huesos y los dientes sanos. El queso también es rico en vitaminas del grupo B, entre las que destaca la vitamina B12, la B9 (ácido fólico), la B1 (tiamina) o la B2 (riboflavina).",
+    category: "Dairy products",
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import "./models/Auth";
 import "./models/User";
 import "./models/Item";
 import "./models/ShopLists";
+import { items } from "../items";
+import { Item } from "./models/Item";
 
 const PORT = process.env.PORT || 5500;
 
@@ -12,6 +14,8 @@ async function main() {
   try {
     /*  await sequelize.authenticate(); */
     /* await sequelize.sync({ alter: true }); */
+    Item.bulkCreate(items)
+
     app.listen(PORT, () => console.log("server running on port :", PORT));
   } catch (error) {
     console.error("Unable to connect to the database:", error);
